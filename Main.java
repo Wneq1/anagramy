29b89a1c544f983c6ff0cc942f90b1fc8a9a3eb7
Wneq1 <127328405+Wneq1@users.noreply.github.com>
2023-06-06 15:43:44 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+ Scanner scan = new Scanner(System.in);
+    
+ System.out.println("Podaj 1-słowo: ");
+    String str1 = scan.nextLine();
+ System.out.println("Podaj 2-słowo: ");
+    String str2 = scan.nextLine();
+    
+    str1 = str1.toLowerCase();
+    str2 = str2.toLowerCase();
+    
+    if(str1.length() == str2.length()) {
+
+      char[] charArray1 = str1.toCharArray();
+      char[] charArray2 = str2.toCharArray();
+
+      Arrays.sort(charArray1);
+      Arrays.sort(charArray2);
+
+      boolean result = Arrays.equals(charArray1, charArray2);
+
+      if(result) {
+        System.out.println(str1 + " i " + str2 + " są anagramami.");
+      }
+      else {
+        System.out.println(str1 + " i " + str2 + " nie są anagramami.");
+      }
+    }
+    else {
+      System.out.println(str1 + " i " + str2 + " nie są anagramami.");
+    }
+  }
+}
+
+
+ 
